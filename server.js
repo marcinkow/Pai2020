4986d17c28e5073e07f94f627fa6a5e8610b840d
Marcin <marcinkow1619@gmail.com>
2020-11-03 18:36:54 +0100

    -powykladowy

--- server.js
+++ server.js
@@ -3,28 +3,60 @@
 
 var http = require('http')
 var static = require('node-static')
+var queryString = require('query-string');
 
 
 var httpServer = http.createServer();
 var fileServer = new static.Server('./public')
 
 var serveError = (res, code ) =>{
-    res.writeHead(code, {"Content-Type": 'text/plain; charset=utf-8'})
+    res.writeHead(code, {'Content-Type': 'text/plain; charset=utf-8'})
     res.write('Error')
     res.end()
 
 }
 
 
-httpServer.on('request', function (req, res){
-        console.log(req.method , req.url)
+var person = {
+    firstName: 'Marcin',
+    lastName: 'Kowalczyk',
+    year: 1998 
+}
 
-        if(/^\/rest/.test(req.url)) {
-             serveError(res, 403)
-        }
-        else
-        {
-            fileServer.serve(req, res)        
+httpServer.on('request', (req, res) =>{
+        console.log(req.method , req.url)
+        var q = queryString.parseUrl(req.url);
+        // console.log(q)
+
+
+        switch(req.method){
+        case 'GET': 
+            switch(q.url){
+            case '/get':
+                res.writeHead(200, {'Content-Type': 'application/json ; charset=utf-8'})
+                res.write(JSON.stringify(person))
+                res.end()
+                break
+            case '/set':
+                person.firstName = q.query.firstName;
+                person.lastName = q.query.lastName;
+                person.year = parseInt(q.query.year);
+                if(isNaN(person.year)) person.year= 1970  
+                res.writeHead(200, {'Content-Type': 'application/json ; charset=utf-8'})
+                res.write(JSON.stringify(person))
+                res.end()
+                break
+            default: 
+                fileServer.serve(req, res)         
+                break
+            }
+        break
+        case 'POST': 
+            serveError(res, 404)
+        break
+        default:
+            serveError(res, 404)
+                
         }
     }
 )
